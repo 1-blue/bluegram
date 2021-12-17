db5164d2cd1e54abd9e8de0093e8e52b71ba90d7
1-blue <63289318+1-blue@users.noreply.github.com>
2021-12-17 09:54:33 +0900

    backend) initial setting passport and passport-local
    1. passport와 passport-local 기본 세팅

---
+++ backend/passport/index.js
@@ -0,0 +1,25 @@
+import passport from "passport";
+import passportLocalConfig from "./local.js";
+import db from "../models/index.js"
+
+const { User } = db;
+
+const passportConfig = () => {
+  passport.serializeUser((user, done) => {
+    done(null, user._id);
+  });
+  
+  passport.deserializeUser(async (_id, done) => {
+    try {
+      const user = await User.findone({ where: { _id } });
+      done(null, user);
+    } catch (error) {
+      console.error("deserializeUser >> ", error);
+      done(error)
+    }
+  });
+  
+  passportLocalConfig();
+}
+
+export default passportConfig;
