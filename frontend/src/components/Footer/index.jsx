fb541c18efe4d2d30e3db456d7ff99cf58a56e5f
1-blue <63289318+1-blue@users.noreply.github.com>
2022-01-25 01:59:35 +0900

    Feat: Add Footer
    웹페이지의 Footer 추가

--- frontend/src/components/Footer/index.jsx
+++ frontend/src/components/Footer/index.jsx
@@ -6,10 +6,29 @@ import { Wrapper } from "./style";
 const Footer = () => {
   return (
     <Wrapper>
-      <h1>Footer</h1>
-      <a href="https://github.com/1-blue/bluegram" target="_blank">
-        <b>GitHub</b>
-      </a>
+      <h2 className="footer-title">공부용으로 만든 인스타그램 클론 사이트</h2>
+      <ul>
+        <li className="footer-link-list">
+          <a href="https://github.com/1-blue/bluegram" target="_blank" className="footer-link">
+            <b>GitHub</b>
+          </a>
+        </li>
+        <li className="footer-link-list">
+          <a href="https://trello.com/b/ACPLTebc/bluegram-v200" target="_blank" className="footer-link">
+            <b>Trello</b>
+          </a>
+        </li>
+        <li className="footer-link-list">
+          <a href="mailto:1-blue98@naver.com" className="footer-link">
+            <b>Email</b>
+          </a>
+        </li>
+        <li className="footer-link-list">
+          <a href="https://velog.io/@1-blue/series/bluegram" target="_blank" className="footer-link">
+            <b>velog</b>
+          </a>
+        </li>
+      </ul>
     </Wrapper>
   );
 };
--- frontend/src/components/Footer/style.js
+++ frontend/src/components/Footer/style.js
@@ -1,5 +1,38 @@
 import styled from "styled-components";
 
 export const Wrapper = styled.footer`
-  margin-bottom: 8vh;
+  padding: 4vh 0 10vh;
+  margin-bottom: 7vh;
+  background: #272727;
+  text-align: center;
+
+  /* footer의 제목 */
+  & .footer-title {
+    color: white;
+    margin-bottom: 4vh;
+  }
+
+  /* footer의 링크들을 감싸는 li */
+  & .footer-link-list {
+    display: inline-block;
+
+    &::before {
+      content: " | ";
+      margin: 0 1em;
+      color: white;
+    }
+    &:first-child::before {
+      content: "";
+      margin: 0;
+    }
+  }
+
+  /* footer의 링크들 */
+  & .footer-link {
+    color: white;
+  }
+
+  @media (min-width: 1024px) {
+    margin-bottom: 0;
+  }
 `;
